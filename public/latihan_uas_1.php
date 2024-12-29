dc7fe0d3361b2f7a7c1ebb8af78a68d3890a81d0
bk1784 <futonabuu456@gmail.com>
2024-12-30 01:03:42 +0700

    mengirim data baru dan mengganti isi body dengan data lain

--- public/latihan_uas_1.php
+++ public/latihan_uas_1.php
@@ -3,14 +3,25 @@
 
 $url = 'https://jsonplaceholder.typicode.com/posts';
 
+// data yang akan dikirim
+$data = array(
+    'title' => 'Belajar Bersama Bagus Kurniawan',
+    'body' => 'belajar bersama specialis cyber security red team, enjoy the hacking',
+    'userId' => 3 //untuk mengirim data baru ke api yang sama cukup ubah data array ini
+
+);
 
 //inisialisasi curl
 $ch = curl_init();
 
-//set opsi untuk metode get
+//set opsi untuk metode POST
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content_Type:application/json'));
 
+//set agar hasil dikembalikan sebagai string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 //ekesekusi curl
 $response = curl_exec($ch);
@@ -18,12 +29,8 @@ $response = curl_exec($ch);
 //menutup curl
 curl_close($ch);
 
-$data = json_decode($response, true);
-
-$items = array_slice($data, 0, 5);
-
 //menampilkan hasil data
-print_r($items );
+echo $response;
 
 
 ?>
