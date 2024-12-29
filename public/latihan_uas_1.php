8f53a5ece4770e4d85912589c8638971a4a6598c
bk1784 <futonabuu456@gmail.com>
2024-12-30 00:20:21 +0700

    mengirim data menggunakan metode POST

--- public/latihan_uas_1.php
+++ public/latihan_uas_1.php
@@ -2,11 +2,23 @@
 
 $url = 'https://jsonplaceholder.typicode.com/posts';
 
+// data yang akan dikirim (format array php)
+$data = array(
+    'title' => 'belajar api dengan PHP',
+    'body' => 'ini adalah contoh penggunaan post request',
+    'userId' => 1 
+);
+
 //inisialisasi curl
 $ch = curl_init();
 
-//eksekusi curl untuk mengambil dengan metode get
+//set opsi untuk metode post dan kirim data
 curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+//set agar hasil dikembalikan sebagai string
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 //ekesekusi curl
@@ -15,11 +27,8 @@ $response = curl_exec($ch);
 //menutup curl
 curl_close($ch);
 
-//mengubah json menajdi array
-$data = json_decode($response, true);
-
 //menampilkan hasil data
-print_r($data);
+echo $response;
 
 
 ?>
